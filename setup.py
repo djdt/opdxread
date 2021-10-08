cfa3d555469fb6ec4451f3d45bde7acc5c8d83a2
djdt <10266332+djdt@users.noreply.github.com>
2021-10-08 21:22:34 +1100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="opdxread",
-    version="0.1.0",
+    version="0.1.1",
     description="Lib for reading Vision64 profilometry data from .OPDx files.",
     packages=["opdxread"],
     license="LGPL",
